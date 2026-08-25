91aa5e779d773479f5bd1d2143c5fcf86e564886
Laboratorios Chillan <chilab@inacap.cl>
2026-08-25 09:52:32 -0400

    falta hacer la cuestion bien XD

--- Ejemplo01/settings.py
+++ Ejemplo01/settings.py
@@ -49,8 +49,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'app1',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'Ejemplo01.urls'
